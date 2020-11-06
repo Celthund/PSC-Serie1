f920b32e4434c2806924c3a7c44f3363851c91a0
Tiago Silva <tiagosik1994@gmail.com>
2020-11-06 22:39:05 +0000

    Add exercise 1.
    Considere o critério “complemento para 2” na representação de valores inteiros relativos (com sinal). Escreva a função max_value, que retorna o maior valor positivo que pode ser armazenado pelo número de bits equivalente ao número de bits de um char, vezes o valor indicado pelo parâmetro nchars. Se o valor não for representável num unsigned long, deve retornar zero. Valorizam-se implementações que tenham em conta o número de bits por char indicado pela macro CHAR_BIT.
    
    unsigned long max_value(size_t nchars);

---
+++ exercicio1.c
@@ -0,0 +1,18 @@
+#include <math.h>
+#include <limits.h>
+#include <stdio.h>
+
+unsigned long max_value(size_t numberChars){
+    int charBit = CHAR_BIT;
+    int longBit = sizeof(unsigned long) * charBit;
+
+    // If max value is not representable in unsigned long size return 0
+    if (longBit < charBit * numberChars) return 0;
+    // Return Two's complement max positive value
+    return (pow(2, charBit * numberChars)) / 2 - 1;
+}
+
+int main (){
+    printf("%lu\n", max_value(1));
+    return 0;
+}
