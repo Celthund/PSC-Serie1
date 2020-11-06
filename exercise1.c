467d4cd18a3219f37c38d1b13c221af2c55154b1
Tiago Silva <tiagosik1994@gmail.com>
2020-11-06 22:52:03 +0000

    Renamed exercicio1.c and exercicio4.c to exercise1.c and exercise4.c corresponding.

--- exercise1.c
+++ exercise1.c
@@ -6,9 +6,9 @@ unsigned long max_value(size_t numberChars){
     int charBit = CHAR_BIT;
     int longBit = sizeof(unsigned long) * charBit;
 
-    // If max value is not representable in unsigned long size return 0
+    // If max value is not representable in unsigned long size return 0.
     if (longBit < charBit * numberChars) return 0;
-    // Return Two's complement max positive value
+    // Return Two's complement max positive value.
     return (pow(2, charBit * numberChars)) / 2 - 1;
 }
 
