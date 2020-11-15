afc409fbd9d32ca5fdf76f6ebffba5e52f5eb708
Tiago Silva <tiagosik1994@gmail.com>
2020-11-15 21:29:10 +0000

    fix some warnings warning: format '%d' expects argument of type 'int', but argument 2 has type 'size_t' {aka 'long long unsigned int'} warning: suggest parentheses around comparison in operand of '|'

--- exercise3.c
+++ exercise3.c
@@ -6,7 +6,7 @@ int isseperator(char c){
     return isspace(c) | c == ',';
 }
 
-size_t string_split(char *text, char *words[], size_t words_size){
+int string_split(char *text, char *words[], size_t words_size){
     char *begin_word = &text[0], *current_char = &text[0];
     int found_words = 0;
     while(*current_char != '\0' && found_words < words_size){
@@ -27,6 +27,6 @@ size_t string_split(char *text, char *words[], size_t words_size){
 int main (){
     char text[] = "Teste, um dois tres \n era uma vez \t.";
     char *words[10] = {0};
-    printf("%d", string_split(&text, &words, sizeof(words) / sizeof(words[0])));
+    printf("%d", string_split(text, words, sizeof(words) / sizeof(words[0])));
     return 0;
 }
